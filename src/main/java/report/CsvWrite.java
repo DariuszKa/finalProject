8fe8d42dc21599a524ee1fa54ed8f78e8c772935
Lukasz Ludwin <gorli@interia.pl>
2017-07-02 11:32:05 +0200

    csv writer second

--- src/main/java/report/CsvWrite.java
+++ src/main/java/report/CsvWrite.java
@@ -10,25 +10,36 @@ public class CsvWrite {
 	
 	
 	
-	public static void main(String[] args) throws IOException {
-	ArrayList<ArrayList<String>> lista = new ArrayList<>();
+	public static void printCsv(ArrayList<ArrayList<String>> lista, String path) throws IOException {
+//	ArrayList<ArrayList<String>> lista = new ArrayList<>();
+//	ArrayList<String> elementListy = new ArrayList<String>();
+//	elementListy.add("Maj");
+//	elementListy.add("Jan Kowalski");
+//	elementListy.add("200");
+//	lista.add(elementListy);
+//	lista.add(elementListy);
 
 		String text = "Hello world";
         BufferedWriter output = null;
+        FileWriter writer = null;
         try {
-            File file = new File("example.txt");
-            output = new BufferedWriter(new FileWriter(file));
-            
-            for (String string : lista) {
-				
+        	 File file =new File(path);
+             file.createNewFile();
+             System.out.println("File is created");
+             writer = new FileWriter(file);
+             
+            for (ArrayList<String> li : lista) {
+            	 writer.write(String.join(", ", li)+"\n"); 
+            	 
 			}
             
-            output.write(text);
+     
         } catch ( IOException e ) {
             e.printStackTrace();
         } finally {
-          if ( output != null ) {
-            output.close();
+          if ( writer != null ) {
+        	  writer.flush();
+            writer.close();
           }
         }
 	}
---
+++ src/test/java/report/CsvWriteTest.java
@@ -0,0 +1,29 @@
+package report;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CsvWriteTest {
+	public static ArrayList<ArrayList<String>> lista;
+	String path = "test1.csv";
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		lista = new ArrayList<>();
+		ArrayList<String> elementListy = new ArrayList<String>();
+		elementListy.add("Maj");
+		elementListy.add("Jan Kowalski");
+		elementListy.add("200");
+		lista.add(elementListy);
+		lista.add(elementListy);
+		
+	}
+
+	@Test
+	public void test() throws IOException {
+		CsvWrite.printCsv(lista, path);
+	}
+
+}
