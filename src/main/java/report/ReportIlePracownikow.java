328f005f88eab76f3ec05b2eb9883e464b8cca18
Pawel Mroz <pawelmroz@gmail.com>
2017-07-02 13:50:16 +0200

    dodany raport 3B

--- src/main/java/report/ReportIlePracownikow.java
+++ src/main/java/report/ReportIlePracownikow.java
@@ -1,10 +1,18 @@
 package report;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public interface ReportIlePracownikow {
 	
-	public default void createReportIlePracownikow(App data) {
-		System.out.println("Creation of report:");
-		System.out.println("...to be implemented by YOU!");
+	public default void createReportIlePracownikow(App data) throws IOException {
+		ArrayList<ArrayList<String>> lista;
+		Report3B testReport = new Report3B(data.getReportList());
+		testReport.prepareReport3B();
+		lista = testReport.returnReport3B();
+		testReport.printReport3B();
+		String path = "test2.csv";
+		CsvWrite.printCsv(lista, path);
 	}
 
 }
